189b1bb117b1370710dc8e9400b48a9c11f791d7
James Anderson <james.r.andreson@utah.edu>
2014-07-02 12:58:52 -0700

    Allow importing specific sections from volumes

--- nornir_web/volume_server/management/commands/volume_import.py
+++ nornir_web/volume_server/management/commands/volume_import.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from nornir_djangomodel.models import *
 import nornir_djangomodel.import_xml as import_xml
+from nornir_shared.argparse_helpers import NumberList
 import os
 
 class Command(BaseCommand):
@@ -11,6 +12,10 @@ class Command(BaseCommand):
         if len(args) == 0:
             raise CommandError("import directory not specified")
 
+        if len(args) == 2:
+            section_list = NumberList(args[1])
+            print("Importing only sections: %s" % str(section_list))
+
         for volume_path in args:
             if not os.path.exists(volume_path):
                 raise CommandError("Requested import directory does not exist:\n\t" + os.path.abspath(volume_path))
@@ -19,6 +24,6 @@ class Command(BaseCommand):
             if not os.path.exists(volume_path):
                 raise CommandError("Requested import file does not exist:\n\t" + os.path.abspath(volume_xml_path))
 
-            dataset_name = import_xml.VolumeXMLImporter.Import(volume_xml_path)
+            dataset_name = import_xml.VolumeXMLImporter.Import(volume_xml_path, section_list=section_list)
 
             self.stdout.write('Successfully imported dataset %s from %s' % (dataset_name, volume_path))
