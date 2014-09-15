2d3c3b3ad8d1b332d31b87a44ec87450b2ca7768
janderson <james.r.anderson@Utah.edu>
2014-09-15 09:58:47 -0700

    Various bugfixes for building section tiles

--- nornir_web/volume_server/management/commands/volume_precache.py
+++ nornir_web/volume_server/management/commands/volume_precache.py
@@ -28,14 +28,16 @@ class Command(BaseCommand):
           
         section_list = None
         if 'sections' in options:
-            section_list = NumberList(options['sections'])
+            if not options['sections'] is None:
+                section_list = NumberList(options['sections'])
             #if len(args) == 2:
             #section_list = NumberList(args[1])
             print("Importing only sections: %s" % str(section_list))
             
         downsample_list = None
         if 'levels' in options:
-            downsample_list = NumberList(options['levels'])
+            if not options['levels'] is None:
+                downsample_list = NumberList(options['levels'])
             #if len(args) == 2:
             #section_list = NumberList(args[1])
             print("Building downsample levels: %s" % str(downsample_list))            
@@ -58,7 +60,7 @@ class Command(BaseCommand):
                 raise ValueError("No coordinate space with name %s" % (coord_space_name))
             
             region = coord_space.GetBounds()
-            section_list = range(region[iBox.minZ], region[iBox.maxZ]+1)
+            section_list = range(int(region[iBox.MinZ]), int(region[iBox.MaxZ])+1)
           
         for section_number in section_list:
             tile_builder = TileBuilder(coord_space_name, section_number, channel_name, filter_name)
@@ -94,6 +96,8 @@ class TileBuilder(object):
         
     def CarryImageToNextDownsampleLevel(self, input_image, input_downsample_level, output_downsample_level):
         
+        print('Section #%d: Copy image from level %d to %d' % (self.section_number, input_downsample_level, output_downsample_level))
+        
         tile_path = self.GetTilePaths(output_downsample_level)
         if not os.path.exists(tile_path):
             os.makedirs(tile_path)
@@ -106,6 +110,9 @@ class TileBuilder(object):
         
     
     def BuildImageForLevel(self, downsample_level):
+        
+        print('Section #%d: Build image for level %d' % (self.section_number, downsample_level))
+        
         coord_space =  CoordSpace.objects.get(name=self.coord_space_name)
         resolution = VOLUME_SERVER_COORD_SPACE_RESOLUTION * downsample_level
          
@@ -124,6 +131,9 @@ class TileBuilder(object):
         for iLevel, level in enumerate(downsample_levels):
             section_image = self.BuildTilesForLevel(level)
             
+            if section_image is None:
+                continue
+            
             if iLevel + 1 < len(downsample_levels):
                 self.CarryImageToNextDownsampleLevel(section_image, level, downsample_levels[iLevel+1])
             
@@ -132,9 +142,7 @@ class TileBuilder(object):
         
         
     def BuildTilesForLevel(self, downsample_level):
-        
-        print('Assemble section #%d' % (self.section_number))
-        
+         
         tile_shape = [VOLUME_SERVER_TILE_HEIGHT, VOLUME_SERVER_TILE_WIDTH] 
         #Check for a marker file indicating we've built tiles 
     
@@ -146,15 +154,20 @@ class TileBuilder(object):
         
         if not os.path.exists(full_image_path): 
             section_image = self.BuildImageForLevel(downsample_level)
+            if section_image is None:
+                print('Section #%d: Unable to generate image for level %d' % (self.section_number, downsample_level))
+        
+                return
+            
             core.SaveImage(full_image_path, section_image)
         else:
             section_image = core.LoadImage(full_image_path)
 
-        print('Creating tiles for section #%d' % (self.section_number))
+        print('Section #%d: Create tiles for level %d' % (self.section_number, downsample_level))
         #Cut apart the full image to create tiles
         tiles = core.ImageToTiles(section_image, tile_size=tile_shape)
-
-        print('Saving tiles for section #%d' % (self.section_number)) 
+        
+        print('Section #%d: Save tiles for level %d' % (self.section_number, downsample_level)) 
         SaveTileGrid(tiles, tile_path)
 
         return section_image
